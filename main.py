0c88183b6b23646d1861b9721bfb7cc600a46914
indie-dev <azweekly99@gmail.com>
2019-11-22 03:55:13 -0500

    Fixed string byte conversion issue
    So apparently 'rb' can fail in reading bytes, and a simple fix is checking if the bit we got is a string (and if it is) we use ord to convert the 'bit' to an actual bit

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from compression import *
 
 __arguments = sys.argv
 __initial_argument = __arguments[1]
+print(__initial_argument)
 if(__initial_argument.lower() is "--upload-to-network"):
     print("Uploading %s to Station network"%(__arguments[2]))
 elif(__initial_argument.lower() is "--download-from-network"):
     print("Downloading unique id %s from Station network"%(__arguments[2]))
+elif(__initial_argument.lower() =="--test"):
+    __compression = Compressor("main.xml")
+    __compression.decompress()
 else:
     print("%s is not a valid parameter"%(__initial_argument))
--- station.cpp
+++ station.cpp
@@ -6,6 +6,8 @@ int main(int argc, char **argv)
     std::string commands = "python main.py ";
     for(int i = 0; i < argc; i++)
     {
+        if(i == 0)
+            i += 1;
         commands += argv[i];
         commands += " ";
     }
